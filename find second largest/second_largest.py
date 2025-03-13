329dcb3a0de1123a8256a518931cbb7ea361db76
Pradeep Kumar Maurya <pk454649@gmail.com>
2025-03-13 22:10:53 +0530

    rectified edge case

--- find second largest/second_largest.py
+++ find second largest/second_largest.py
@@ -1,6 +1,7 @@
 def find_second_largest(arr):
     max = float('-inf')
-    second_largest = float('-inf')
+    second_max = float('-inf')
+    second_max_index = None
 
     for num in arr:
         if num > max:
@@ -9,13 +10,14 @@ def find_second_largest(arr):
     for i in range(len(arr)):
         diff = arr[i] - num
 
-        if diff > second_largest and arr[i] != max:
-            second_largest = arr[i]
+        if (diff > second_max and arr[i] != max):
+            second_max = diff
+            second_max_index = i
 
-    return second_largest
+    return arr[second_max_index]
 
 
 arr = [1, 2, 4, 3, 2, 1]
-arr = [4, 3, 2, 1]
+arr = [4, 3, 2, 1, 5, 6, 7, 0, 0, 8]
 arr = [-4, -3, -2, -1, 0, 1]
 print(find_second_largest(arr))
