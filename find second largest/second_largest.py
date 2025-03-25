fd257a6c8f018ba0fceefcd34971ede71602f9c9
Pradeep Kumar Maurya <pk454649@gmail.com>
2025-03-25 22:51:30 +0530

    code

--- find second largest/second_largest.py
+++ find second largest/second_largest.py
@@ -3,6 +3,9 @@ def find_second_largest(arr):
     second_max = float('-inf')
     second_max_index = None
 
+    if len(arr) == 1:
+        return -1
+
     for num in arr:
         if num > max:
             max = num
@@ -20,4 +23,8 @@ def find_second_largest(arr):
 arr = [1, 2, 4, 3, 2, 1]
 arr = [4, 3, 2, 1, 5, 6, 7, 0, 0, 8]
 arr = [-4, -3, -2, -1, 0, 1]
+arr = [4, 3, 2, 1]
+arr = [1, 2, 3, 4, 5]
+arr = [1, 2, 4, 7, 7, 5]
+arr = [1]
 print(find_second_largest(arr))
